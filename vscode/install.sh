246559bd876bb91431f7fdc08f77ac12ca07982a
Ole Christian Slaattene <slaattene@gmail.com>
2019-12-16 10:53:33 +0100

    Add vscode extension install script

---
+++ vscode/install.sh
@@ -0,0 +1,4 @@
+code --install-extension pkief.material-icon-theme
+code --install-extension alan.stylus
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension dracula-theme.theme-dracula
