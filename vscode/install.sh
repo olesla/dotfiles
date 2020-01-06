e1d7872e656008ebd2430dcafdc284d749a46c19
Ole Christian Slaattene <slaattene@gmail.com>
2020-01-06 09:41:47 +0100

    Add vscode extensions to install script

--- vscode/install.sh
+++ vscode/install.sh
@@ -2,3 +2,5 @@ code --install-extension pkief.material-icon-theme
 code --install-extension alan.stylus
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension dracula-theme.theme-dracula
+code --install-extension firefox-devtools.vscode-firefox-debug
+code --install-extension eriklynd.json-tools
