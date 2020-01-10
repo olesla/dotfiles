a4e77bbecc740d55096ae5a532fdf571be85204e
Ole Christian Slaattene <slaattene@gmail.com>
2020-01-10 15:53:10 +0100

    add todo+

--- vscode/install.sh
+++ vscode/install.sh
@@ -4,3 +4,5 @@ code --install-extension dbaeumer.vscode-eslint
 code --install-extension dracula-theme.theme-dracula
 code --install-extension firefox-devtools.vscode-firefox-debug
 code --install-extension eriklynd.json-tools
+code --install-extension fabiospampinato.vscode-todo-plus
+
