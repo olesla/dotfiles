28603a74b45e709af83a67883148c1298f056344
Ole Christian Slaattene <slaattene@gmail.com>
2020-01-24 16:01:28 +0100

    change darcula with material color theme

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,7 +1,7 @@
 code --install-extension pkief.material-icon-theme
 code --install-extension alan.stylus
 code --install-extension dbaeumer.vscode-eslint
-code --install-extension dracula-theme.theme-dracula
+code --install-extension equinusocio.vsc-material-theme
 code --install-extension firefox-devtools.vscode-firefox-debug
 code --install-extension eriklynd.json-tools
 code --install-extension fabiospampinato.vscode-todo-plus
