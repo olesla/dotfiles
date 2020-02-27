e7088e970de2b8702affa5958ee0e448f1959d0a
Ole Christian <slaattene@gmail.com>
2020-02-27 22:02:41 +0100

    add man color function, add old stuff from zsh to config.fish, add fish_variables

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,19 @@
+#------------
+# General
+#------------
+
+# Keyboard
+xset r rate 220 40
+
+# History
+set HISTSIZE 1000
+set SAVEHIST 1000
+set HISTFILE "$HOME/.bash_history"
+
+#------------
+# Aliases
+#------------
+alias l="ls -alh"
+alias c="clear"
+alias gs="git status"
+alias code="codium"
