b26f20b69675aa2d56ae0703e2bf9f85a4ac04f0
Ole Christian <slaattene@gmail.com>
2020-03-07 17:37:50 +0100

    add dark+ contrast theme

--- codium/install.sh
+++ codium/install.sh
@@ -1,3 +1,3 @@
-code --install-extension pkief.material-icon-theme
-code --install-extension dbaeumer.vscode-eslint
-
+codium --install-extension pkief.material-icon-theme
+codium --install-extension dbaeumer.vscode-eslint
+codium --install-extension k3a.theme-dark-plus-contrast
