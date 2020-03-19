4e0074d7d946fd62995b39535a3ee1cc7051f269
Ole Christian <slaattene@gmail.com>
2020-03-19 21:28:55 +0100

    Replace theme with slime theme

--- codium/install.sh
+++ codium/install.sh
@@ -1,3 +1,3 @@
 codium --install-extension pkief.material-icon-theme
 codium --install-extension dbaeumer.vscode-eslint
-codium --install-extension k3a.theme-dark-plus-contrast
+codium --install-extension smlombardi.slime
