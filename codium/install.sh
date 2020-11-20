bdec4ad2b8ea4471bd95d508ce8920696103bdb4
Ole Christian Slaattene <slaattene@gmail.com>
2020-11-20 15:58:16 +0100

    add some changes

--- codium/install.sh
+++ codium/install.sh
@@ -1,8 +1,12 @@
+#!/usr/bin/bash
+
+# extensions
 codium --install-extension dbaeumer.vscode-eslint
 codium --install-extension bmalehorn.vscode-fish
 codium --install-extension sysoev.language-stylus
 codium --install-extension vscodevim.vim
-# Color themes
+
+# colors
 codium --install-extension muhammad-sammy.gray-theme
 codium --install-extension vladeeg.vscode-theme-vlight
 codium --install-extension thenikso.github-plus-theme
